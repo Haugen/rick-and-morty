eb78eab21d2175a29ee18c424f32451b5b5c8689
Tobias Haugen <hej@tobiashaugen.se>
2019-03-03 13:52:06 -0500

    Moved logic from episode component to episode page container.

--- src/components/Episode/index.js
+++ src/components/Episode/index.js
@@ -1,33 +1,13 @@
-import React, { useState, useEffect } from 'react';
-
-import cFetch from '../../utility/fetch';
-import { getIdsFromUrl } from '../../utility/helpers';
+import React from 'react';
 
 const Episode = ({ episodeData }) => {
-  const [characters, setCharacters] = useState(null);
-
-  useEffect(() => {
-    fetchCharacters();
-  }, []);
-
-  async function fetchCharacters() {
-    const characterIds = getIdsFromUrl(episodeData.characters);
-    const fetchedCharacters = await cFetch('/character/' + characterIds);
-    if (!fetchedCharacters.hasError) {
-      setCharacters(fetchedCharacters);
-    }
-  }
-
-  return (
+  return !episodeData ? (
+    'Loading episode...'
+  ) : (
     <>
       <article className="episode">
         <h2>{episodeData.name}</h2>
       </article>
-      {!characters
-        ? 'Loading characters...'
-        : characters.map(character => {
-            return character.name;
-          })}
     </>
   );
 };
--- src/containers/EpisodePage/index.js
+++ src/containers/EpisodePage/index.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from 'react';
 
 import cFetch from '../../utility/fetch';
 import Episode from '../../components/Episode';
+import CharacterList from '../../components/CharacterList';
+import { getIdsFromUrl } from '../../utility/helpers';
 
 const EpisodePage = ({ match }) => {
   const [episode, setEpisode] = useState(null);
+  const [characters, setCharacters] = useState(null);
 
   useEffect(() => {
     fetchEpisode(match.params.id);
-  }, []);
+    if (episode) fetchCharacters(episode);
+  }, [episode]);
 
   async function fetchEpisode(episodeId) {
     const fetchedEpisode = await cFetch('/episode/' + episodeId);
@@ -17,7 +21,20 @@ const EpisodePage = ({ match }) => {
     }
   }
 
-  return !episode ? 'Loading...' : <Episode episodeData={episode} />;
+  async function fetchCharacters(fetchedEpisode) {
+    const characterIds = getIdsFromUrl(fetchedEpisode.characters);
+    const fetchedCharacters = await cFetch('/character/' + characterIds);
+    if (!fetchedCharacters.hasError) {
+      setCharacters(fetchedCharacters);
+    }
+  }
+
+  return (
+    <>
+      <Episode episodeData={episode} />
+      <CharacterList characters={characters} />
+    </>
+  );
 };
 
 export default EpisodePage;
