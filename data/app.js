27d6f47a62e160ea06839cca5e9ff2fcb7f3ab7b
Tobias Haugen <hej@tobiashaugen.se>
2019-03-04 10:41:48 -0500

    Finnished and fixed the mock API.

--- data/app.js
+++ data/app.js
@@ -4,12 +4,23 @@ const express = require('express');
 
 const app = express();
 
-app.use('/episodes', (req, res) => {
+app.use('*', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  next();
+});
+
+app.use('/episode/:id', (req, res) => {
+  const episode = JSON.parse(fs.readFileSync(__dirname + '/episode.json'));
+  res.json(episode);
+});
+
+app.use('/episode', (req, res) => {
   const episodes = JSON.parse(fs.readFileSync(__dirname + '/episodes.json'));
   res.json(episodes);
 });
 
-app.use('/characters', (req, res) => {
+app.use('/character', (req, res) => {
   const characters = JSON.parse(
     fs.readFileSync(__dirname + '/characters.json')
   );
